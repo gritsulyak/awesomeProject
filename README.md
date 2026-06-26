a383cad281b8ef4f954e81d8ebc459f47ea1a6e9
Roman Gritsulyak <4rtgcom@gmail.com>
2026-06-26 16:11:25 +0300

    vegeta tested

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ docker compose up
 ab -n 300 -c 10 http://127.0.0.1:10080/api/v1/satellite/moon
 
 k6 run scripts/k6/script.js
+
+$ echo "GET http://127.0.0.1:10080/api/v1/satellite/moon" | vegeta attack -rate=10000 -duration=5s | vegeta report 
+Requests      [total, rate, throughput]         50000, 10000.39, 9999.95
+Duration      [total, attack, wait]             5s, 5s, 218.436µs
+Latencies     [min, mean, 50, 90, 95, 99, max]  156.628µs, 1.289ms, 493.524µs, 646.497µs, 1.041ms, 32.883ms, 97.569ms
+Bytes In      [total, mean]                     800000, 16.00
+Bytes Out     [total, mean]                     0, 0.00
+Success       [ratio]                           100.00%
+Status Codes  [code:count]                      200:50000  
+Error Set: 
+
+- current state
--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
-	"sync"
 	"time"
 
 	"github.com/gritsulyak/awesomeProject/internal/application"
@@ -26,38 +22,10 @@ func main() {
 		log.Fatalf("failed to start app: %v", err)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	pprofSrv := &http.Server{
-		Addr:    "127.0.0.1:6060",
-		Handler: http.DefaultServeMux, // важно для net/http/pprof [web:114][web:133]
-	}
-
-	// Prefer Listen so we can detect "address already in use" early
-	ln, err := net.Listen("tcp", pprofSrv.Addr)
-	if err != nil {
-		log.Fatalf("pprof listen failed on %s: %v", pprofSrv.Addr, err)
-	}
-
-	go func() {
-		defer wg.Done()
-		log.Printf("pprof: http://%s/debug/pprof/", pprofSrv.Addr)
-
-		if err := pprofSrv.Serve(ln); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Printf("pprof serve error: %v", err)
-		}
-	}()
-
 	<-done
 
 	shutdownCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
 	app.Stop(shutdownCtx)
-
-	if err := pprofSrv.Shutdown(shutdownCtx); err != nil {
-		log.Printf("pprof shutdown: %v", err)
-	}
-	wg.Wait()
 }
